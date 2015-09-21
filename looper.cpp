58f6f6f8cdbabbd8462ac86b5b1d0ce23c9a5376
athairus <athairus@gmail.com>
2015-09-21 15:26:46 -0400

    Increased accuracy at the cost of precision, brought back blur
    ~0.01% accuracy is good enough!

--- looper.cpp
+++ looper.cpp
@@ -8,7 +8,8 @@ void Looper::beginLoop( double interval ) {
 
     QElapsedTimer timer;
     int counter = 1;
-    int printEvery = 1;
+    int printEvery = 240;
+    int yieldCounter = 0;
 
     double timeElapsed = 0.0;
 
@@ -19,9 +20,12 @@ void Looper::beginLoop( double interval ) {
 
             counter++;
             if( counter % printEvery == 0 )  {
+                qDebug() << "Yield() ran" << yieldCounter << "times";
                 qDebug() << "timeElapsed =" << timeElapsed << "ms | interval =" << interval << "ms";
                 qDebug() << "Difference:" << timeElapsed - interval << " -- " << ( ( timeElapsed - interval ) / interval ) * 100.0 << "%";
             }
+
+            yieldCounter = 0;
 
             emit signalFrame();
 
@@ -32,7 +36,8 @@ void Looper::beginLoop( double interval ) {
         timer.start();
 
         // Running this just once means massive overhead from calling timer.start() so many times so quickly
-        for( int i = 0; i < 100; i++ ) {
+        for( int i = 0; i < 1000; i++ ) {
+            yieldCounter++;
             QThread::yieldCurrentThread();
         }
 
